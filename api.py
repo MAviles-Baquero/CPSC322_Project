ffe5481630cc9093bd5d5fc62489a1a2a8518186
Tristan <tcall@zagmail.gonzaga.edu>
2021-05-05 18:47:35 +0000

    Fixed sp issue

--- api.py
+++ api.py
@@ -80,5 +80,5 @@ def get_forest():
  
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
-    app.run(debug=FALSE, host = "0.0.0.0", port=port)
+    app.run(debug=False, host = "0.0.0.0", port=port)
     # app.run(debug=True)
